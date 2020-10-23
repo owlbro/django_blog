701324291854589b0b305d288d56dfc3bbcc7cd1
Аркадий Сыроежко <arkadijsyroezko@iMac-Arkadij.local>
2020-10-23 13:57:21 +0300

    100%

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ from .import views
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    #path('dashboard/edit/', views.dashboard_edit, name='dashboard_edit'),
+    path('dashboard/edit/', views.dashboard_edit, name='dashboard_edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 #from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from blog.models import Post
 from django.utils import timezone
+from .forms import UserForm, ProfileForm
 
 
 class SignUpView(generic.CreateView):
@@ -16,17 +17,20 @@ def dashboard(request):
     last_posts = Post.objects.filter(author_id = request.user).order_by('-published_date')[:5]
     return render(request, 'dashboard.html', {'last_posts' : last_posts})
 
-"""def dashboard_edit(request):
+#@login_required
+#@transaction.atomic
+def dashboard_edit(request):
     if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=user)
-        profile_form = ProfileForm(request.POST, instance=userprofile)
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
-            user_form.save(commit=False)
-            profile_form.save(commit=False)
-            user.save()
-            userprofile.save()
+            user_form.save()
+            profile_form.save()
+            #messages.success(request, _('Ваш профиль был успешно обновлен!'))
             return redirect('dashboard')
+        #else:
+            #messages.error(request, _('Пожалуйста, исправьте ошибки.'))
     else:
-        user_form = UserForm(instance=user)
-        profile_form = ProfileForm(instance=userprofile)
-    return render(request, 'dashboard_edit.html', {'user_form': user_form, 'profile_form': profile_form})"""
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'dashboard_edit.html', {'user_form': user_form, 'profile_form': profile_form})
